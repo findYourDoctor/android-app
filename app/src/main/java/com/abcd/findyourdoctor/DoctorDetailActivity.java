5fad85d23708d1c81af8b2ac0be140d1f8da6721
charusingh1609 <singhcharu359@gmail.com>
2021-07-09 16:39:15 +0530

    --

--- app/src/main/java/com/abcd/findyourdoctor/DoctorDetailActivity.java
+++ app/src/main/java/com/abcd/findyourdoctor/DoctorDetailActivity.java
@@ -3,6 +3,8 @@ package com.abcd.findyourdoctor;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -15,6 +17,11 @@ public class DoctorDetailActivity extends AppCompatActivity {
     private DoctorDetailData doctorDetailData;
     private TextView txtName;
     private ImageView imgProfilePic;
+    private TextView txtExp;
+    private TextView txtLan;
+    private ImageView imgSymbol;
+    private Button btnChat;
+    private Button btnBookSlot;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,21 +31,44 @@ public class DoctorDetailActivity extends AppCompatActivity {
         doctorDetailData = MockRepository.getInstance().getData(this, DoctorDetailData.class, "doctor_detail.json");
         initialiseView();
         setData();
-
+        setOnClickListener();
     }
-
 
     private void initialiseView() {
         txtName = findViewById(R.id.txtName);
         imgProfilePic = findViewById(R.id.imgProfilePic);
+        txtExp = findViewById(R.id.txtExp);
+        txtLan=findViewById(R.id.txtLan);
+        imgSymbol=findViewById(R.id.imgSymbol);
+        btnChat=findViewById(R.id.btnChat);
+        btnBookSlot=findViewById(R.id.btnBookSlot);
     }
 
     private void setData() {
         txtName.setText(doctorDetailData.getName());
         setProfileImage();
+        txtExp.setText(doctorDetailData.getExperience());
+        txtLan.setText(doctorDetailData.getLanguages());
     }
 
     private void setProfileImage() {
         Picasso.get().load(doctorDetailData.getProfilePic()).into(imgProfilePic);
+
+    }
+
+    private void setOnClickListener() {
+        btnBookSlot.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        btnChat.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                
+            }
+        });
+
     }
 }
