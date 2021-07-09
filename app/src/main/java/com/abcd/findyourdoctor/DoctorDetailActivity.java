ec5dbce02449b5343f8ee2c24d6072d2389b7495
charusingh1609 <singhcharu359@gmail.com>
2021-07-09 16:43:53 +0530

    --

--- app/src/main/java/com/abcd/findyourdoctor/DoctorDetailActivity.java
+++ app/src/main/java/com/abcd/findyourdoctor/DoctorDetailActivity.java
@@ -66,7 +66,7 @@ public class DoctorDetailActivity extends AppCompatActivity {
         btnChat.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                
+        
             }
         });
 
