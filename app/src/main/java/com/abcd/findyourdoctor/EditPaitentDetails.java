67eaa70c50cbb37161e01c80d3f2e7b776d9749d
charusingh1609 <singhcharu359@gmail.com>
2021-08-22 00:19:51 +0530

    changes for user detail

---
+++ app/src/main/java/com/abcd/findyourdoctor/EditPaitentDetails.java
@@ -0,0 +1,38 @@
+package com.abcd.findyourdoctor;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
+//import android.widget.Toolbar;
+
+public class EditPaitentDetails extends AppCompatActivity {
+    private Toolbar toolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_paitent_details);
+
+
+
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        MenuInflater inflater=getMenuInflater();
+        inflater.inflate(R.menu.menu,menu);
+        return true;
+    }
+
+
+}
